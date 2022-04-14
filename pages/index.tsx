eda0a32e12aa09f9181d9a05a5600fd5265d512b
jay <dj.min43@gmail.com>
2022-04-14 17:06:19 +0900

    add a menu

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,17 @@ import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
-  return <div></div>;
+  return (
+    <section>
+      <div>
+        <h1>김종찬 만두</h1>
+      </div>
+      <div>
+        <span>고기만두</span>
+        <span>100만원</span>
+      </div>
+    </section>
+  );
 };
 
 export default Home;
