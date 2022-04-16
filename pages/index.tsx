5b1daae54e3da8b8c83be002fa9e3ab60de028d6
jay <dj.min43@gmail.com>
2022-04-16 16:50:15 +0900

    add office coords

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,30 @@ import type { NextPage } from "next";
 import { useEffect, useState } from "react";
 import { useCurrentLocation, useCalculateDistance } from "../hooks";
 
+interface Coords {
+  latitude: number;
+  longitude: number;
+}
+
 const Home: NextPage = () => {
+  const officeCoordinates: Coords = {
+    latitude: 37.517464,
+    longitude: 127.040718,
+  };
   const { currentLatitude, currentLongitude } = useCurrentLocation();
+
+  const distanceToOffice = useCalculateDistance(
+    currentLatitude,
+    currentLongitude,
+    officeCoordinates.latitude,
+    officeCoordinates.longitude
+  );
+
   return (
     <section>
       <p>latitude: {currentLatitude}</p>
       <p>longitude: {currentLongitude}</p>
+      <p>사무실까지: {distanceToOffice}km </p>
     </section>
   );
 };
