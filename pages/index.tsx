aa4fd3ffda3b6b52a55269158c8b84b85471c3cd
jay <dj.min43@gmail.com>
2022-05-05 16:12:44 +0900

    add path for office locator

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,39 +1,7 @@
 import type { NextPage } from "next";
-import { useCurrentLocation, useCalculateDistance } from "../hooks";
-
-interface Coords {
-  latitude: number;
-  longitude: number;
-}
 
 const Home: NextPage = () => {
-  const officeCoordinates: Coords = {
-    latitude: 37.517464,
-    longitude: 127.040718,
-  };
-  const { currentLatitude, currentLongitude } = useCurrentLocation();
-
-  const distanceToOffice = useCalculateDistance(
-    currentLatitude,
-    currentLongitude,
-    officeCoordinates.latitude,
-    officeCoordinates.longitude
-  );
-
-  return (
-    <section>
-      <p>latitude: {currentLatitude}</p>
-      <p>longitude: {currentLongitude}</p>
-      <p>
-        사무실까지:
-        {currentLatitude === 0 && currentLongitude === 0 ? (
-          <span>계산중....</span>
-        ) : (
-          <span>{distanceToOffice}km</span>
-        )}
-      </p>
-    </section>
-  );
+  return <section></section>;
 };
 
 export default Home;
